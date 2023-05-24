407a6c2f206f35dcb679ccd90ae7a1cd056260bf
Sheela Malar <Sheelamalar@gmail.com>
2023-05-24 09:39:02 +0530

    initial commit

---
+++ src/main/java/com/reservation/reservationGateway/ReservationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.reservationGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationGatewayApplication.class, args);
+	}
+
+}
